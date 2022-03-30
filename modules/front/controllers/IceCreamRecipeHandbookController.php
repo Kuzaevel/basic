7f545286d979e563abf8d5650d9484a63f1ec25d
e.kuzyaev <e.kuzyaev@promo-bot.ru>
2022-03-30 09:57:51 +0500

    add controllers for all models

--- modules/front/controllers/IceCreamRecipeHandbookController.php
+++ modules/front/controllers/IceCreamRecipeHandbookController.php
@@ -2,11 +2,8 @@
 
 namespace app\modules\front\controllers;
 
-use Yii;
 use app\components\MyActiveController;
 use app\common\models\IceCreamRecipeHandbook;
-use yii\data\ActiveDataFilter;
-use yii\data\ActiveDataProvider;
 
 class IceCreamRecipeHandbookController extends MyActiveController
 {
--- modules/front/controllers/SizeHandbookController.php
+++ modules/front/controllers/SizeHandbookController.php
@@ -2,11 +2,8 @@
 
 namespace app\modules\front\controllers;
 
-use Yii;
 use app\components\MyActiveController;
 use app\common\models\SizeHandbook;
-use yii\data\ActiveDataFilter;
-use yii\data\ActiveDataProvider;
 
 class SizeHandbookController extends MyActiveController
 {
